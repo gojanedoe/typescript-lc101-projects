65ff402be56012120593db333743aa067f1ab8b5
Jane Doe <77185646+gojanedoe@users.noreply.github.com>
2021-06-16 12:50:35 -0500

    Add compiled JS files

---
+++ studio/Rocket.js
@@ -0,0 +1,51 @@
+"use strict";
+exports.__esModule = true;
+exports.Rocket = void 0;
+var Rocket = /** @class */ (function () {
+    function Rocket(name, totalCapacityKg) {
+        this.cargoItems = [];
+        this.astronauts = [];
+        this.name = name;
+        this.totalCapacityKg = totalCapacityKg;
+    }
+    Rocket.prototype.sumMass = function (items) {
+        // returns sum of all items using each item's massKg prop
+        var sum = 0;
+        items.forEach(function (item) { return sum += item.massKg; });
+        return sum;
+    };
+    Rocket.prototype.currentMassKg = function () {
+        var combinedMass = this.sumMass(this.astronauts) + this.sumMass(this.cargoItems);
+        return combinedMass;
+    };
+    Rocket.prototype.canAdd = function (item) {
+        if ((this.currentMassKg() + item.massKg) <= this.totalCapacityKg) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+    Rocket.prototype.addCargo = function (cargo) {
+        // See if another item can be added
+        if (this.canAdd(cargo)) {
+            this.cargoItems.push(cargo);
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+    Rocket.prototype.addAstronaut = function (astronaut) {
+        // See if another astronaut can be added
+        if (this.canAdd(astronaut)) {
+            this.astronauts.push(astronaut);
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+    return Rocket;
+}());
+exports.Rocket = Rocket;
